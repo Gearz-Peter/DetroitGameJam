49e0b980cae4ee634f3ce6d7775ffac37bf0b99b
Peter Newman <newmanp5@coventry.ac.uk>
2022-04-27 13:02:23 +0100

    guy sprites

--- DetroitGameJam/Assets/Peter/Scripts/DoEnd.cs
+++ DetroitGameJam/Assets/Peter/Scripts/DoEnd.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DoEnd : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collider.tag == "End")
+        {
+            GameObject.FindWithTag("Player").GetComponent<EnableDisable>().flip = true;
+        }
     }
 }
